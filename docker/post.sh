1e7b8bb4efa8ad61f3c31f8db8dbd2c37b2c594d
Andrea Silvi <andrea.silvi94@gmail.com>
2023-01-31 13:24:56 +0100

    Changed docker

--- docker/post.sh
+++ docker/post.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+docker-compose exec ipfs ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/8081/ws", "/ip6/::/tcp/4001"]' --json
+docker-compose exec ipfs ipfs config --bool Swarm.EnableHolePunching true
+docker-compose exec ipfs ipfs config --bool Swarm.EnableAutoNATService true
+docker-compose exec ipfs ipfs config --bool Swarm.RelayClient.Enabled true
--- docker/pre.sh
+++ docker/pre.sh
@@ -3,7 +3,7 @@
 EMAIL=info@humandataincome.com
 DOMAIN=ipfs.humandataincome.com,ipfs-api.humandataincome.com
 BASIC_USERNAME=hudi
-BASIC_PASSWORD=$(openssl rand -base64 32)
+BASIC_PASSWORD=$(openssl rand -base64 16)
 
 #rm -rf data
 
@@ -12,6 +12,10 @@ mkdir -p data/nginx/
 printf '%s:' "$BASIC_USERNAME" >> data/nginx/.htpasswd
 openssl passwd -apr1 "$BASIC_PASSWORD" >> data/nginx/.htpasswd
 echo "Login with $BASIC_USERNAME and $BASIC_PASSWORD"
+
+mkdir -p "data/certbot/conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "data/certbot/conf/options-ssl-nginx.conf"
+openssl dhparam -out "data/certbot/conf/ssl-dhparams.pem" 2048
 
 docker-compose -f docker-compose.pre.yml down
 export EMAIL=${EMAIL}
