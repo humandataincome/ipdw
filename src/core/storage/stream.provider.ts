4332acdfd0102383290fa015a798f41720ba5bc3
Andrea Silvi <andrea.silvi94@gmail.com>
2023-02-15 13:06:34 +0100

    added chunked read

--- src/core/storage/stream.provider.ts
+++ src/core/storage/stream.provider.ts
@@ -1,8 +1,8 @@
 import {StorageProvider} from "./";
 
 interface Metadata {
-    chunksCount: number;
-    totalLength: number;
+    chunks: number;
+    size: number;
 }
 
 export class StreamProvider {
@@ -20,27 +20,27 @@ export class StreamProvider {
         return this.storage.has(key + "_");
     }
 
-    public async getWritable(key: string, maxChunkSize: number = 131072 /* 128kb default*/, append: boolean = true): Promise<WritableStream> {
-        let metadata = {chunksCount: 0, totalLength: 0};
+    public async getWritable(key: string, maxChunkSize: number = 131072 /* 128kb default*/, append: boolean = false): Promise<WritableStream> {
+        let metadata = {chunks: 0, size: 0};
         let tmp = new Uint8Array(maxChunkSize);
         let tmpSize = 0;
 
         if (await this.storage.has(key + "_")) {
             const tmpMetadata = JSON.parse(new TextDecoder().decode(await this.storage.get(key + "_"))) as Metadata
 
-            if (append && tmpMetadata.chunksCount > 0) {
-                const lastChunk = await this.storage.get(key + "_" + (tmpMetadata.chunksCount - 1));
+            if (append && tmpMetadata.chunks > 0) {
+                const lastChunk = await this.storage.get(key + "_" + (tmpMetadata.chunks - 1));
                 if (lastChunk!.length < maxChunkSize) {
                     tmpSize = lastChunk!.length;
                     tmp.set(lastChunk!, 0);
-                    metadata.chunksCount = tmpMetadata.chunksCount - 1;
-                    metadata.totalLength = tmpMetadata.totalLength - tmpSize;
+                    metadata.chunks = tmpMetadata.chunks - 1;
+                    metadata.size = tmpMetadata.size - tmpSize;
                 } else {
-                    metadata.chunksCount = tmpMetadata.chunksCount;
-                    metadata.totalLength = tmpMetadata.totalLength;
+                    metadata.chunks = tmpMetadata.chunks;
+                    metadata.size = tmpMetadata.size;
                 }
             } else {
-                for (let pivot = tmpMetadata.chunksCount - 1; pivot >= 0; pivot--)
+                for (let pivot = tmpMetadata.chunks - 1; pivot >= 0; pivot--)
                     await this.storage.set(key + "_" + pivot, undefined);
 
                 await this.storage.set(key + "_", undefined);
@@ -60,37 +60,63 @@ export class StreamProvider {
                     writtenSize += writableSize
 
                     if (tmpSize === maxChunkSize) {
-                        metadata.chunksCount++;
-                        metadata.totalLength += tmpSize;
+                        metadata.chunks++;
+                        metadata.size += tmpSize;
                         tmpSize = 0;
 
-                        await _self.storage.set(key + "_" + (metadata.chunksCount - 1), tmp);
+                        await _self.storage.set(key + "_" + (metadata.chunks - 1), tmp);
                         await _self.storage.set(key + "_", new TextEncoder().encode(JSON.stringify(metadata)))
                     }
                 }
             },
             async close() {
                 if (tmpSize > 0) {
-                    metadata.chunksCount++;
-                    metadata.totalLength += tmpSize;
+                    metadata.chunks++;
+                    metadata.size += tmpSize;
 
-                    await _self.storage.set(key + "_" + (metadata.chunksCount - 1), tmp.slice(0, tmpSize));
+                    await _self.storage.set(key + "_" + (metadata.chunks - 1), tmp.slice(0, tmpSize));
                     await _self.storage.set(key + "_", new TextEncoder().encode(JSON.stringify(metadata)))
                 }
             }
         });
     }
 
-    // TODO: implement maxChunkSize logic
     public async getReadable(key: string, maxChunkSize: number = 131072 /* 128kb default*/): Promise<ReadableStream> {
         const metadata = JSON.parse(new TextDecoder().decode(await this.storage.get(key + "_"))) as Metadata;
+        let pivot = {chunk: 0, end: 0};
 
         const _self = this;
         return new ReadableStream<Uint8Array>({
-            async start(controller) {
-                for (let pivot = 0; pivot < metadata.chunksCount; pivot++)
-                    controller.enqueue(await _self.storage.get(key + "_" + pivot));
-                controller.close();
+            async pull(controller) { // Using pull function instead of start to read it asynchronously
+                let tmp = new Uint8Array(maxChunkSize);
+                let tmpSize = 0;
+
+                while (true) {
+                    const chunk = await _self.storage.get(key + "_" + pivot.chunk);
+                    const readableSize = Math.min(maxChunkSize - tmpSize, chunk!.length - pivot.end);
+
+                    const subChunk = chunk!.slice(pivot.end, pivot.end + readableSize);
+                    tmp.set(subChunk, tmpSize);
+
+                    tmpSize += readableSize;
+                    pivot.end += readableSize;
+
+                    if (pivot.end === chunk!.length) {
+                        pivot.chunk++;
+                        pivot.end = 0;
+                    }
+
+                    if (tmpSize === maxChunkSize) {
+                        controller.enqueue(tmp);
+                        break;
+                    }
+
+                    if (pivot.chunk === metadata.chunks) {
+                        controller.enqueue(tmp.slice(0, tmpSize));
+                        controller.close();
+                        break;
+                    }
+                }
             },
         });
     }
